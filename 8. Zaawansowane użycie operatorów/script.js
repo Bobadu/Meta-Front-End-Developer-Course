ea8e3411bd302188ff534ac4fb99248ecd2a9fb8
Bobadu <kacperlesniewski165@gmail.com>
2025-03-26 16:44:17 +0100

    Ex. 8

---
+++ 8. Zaawansowane użycie operatorów/script.js
@@ -0,0 +1,27 @@
+let score = 8
+console.log("Mid-level skills: " + (score > 0 && score < 10))
+
+let timeRemaining = 0
+let energy = 10
+console.log("Game Over: " + (timeRemaining === 0 || energy === 0))
+
+let num1 = 2;
+let num2 = 5;
+let test1 = num1 % 2;
+let test2 = num2 % 2;
+let result1 = test1 === 0;
+let result2 = test2 === 0;
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2);
+
+console.log(5 + 10);
+
+let now = "Now in ";
+let three = 3;
+let d = "D!";
+console.log(now + three + d);
+
+let counter = 0;
+counter += 5;
+counter += 3;
+console.log(counter);
