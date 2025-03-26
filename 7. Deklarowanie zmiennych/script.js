a3a9ba435a5ab63ce2f7d7aece44b3f8a98555d9
Bo ba du <kacperlesniewski165@gmail.com>
2025-03-26 13:51:02 +0100

    Labs and Exc. 1-7

---
+++ 7. Deklarowanie zmiennych/script.js
@@ -0,0 +1,27 @@
+// Declare variables for pet names
+let petDog = "Rex";
+let petCat = "Pepper";
+
+// Log individual variables
+console.log(petDog);
+console.log(petCat);
+
+// Log text along with the variables
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+
+// Declare variables for animal sounds
+let catSound = "purr";
+let dogSound = "woof";
+
+// Log petDog with its sound
+console.log(petDog, "says", dogSound);
+
+// Log petCat with its sound
+console.log(petCat, "says", catSound);
+
+// Reassign the catSound variable
+catSound = "meow";
+
+// Log updated message for petCat
+console.log(petCat, "now says", catSound);
