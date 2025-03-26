cf4b9adbbcf693bf7ff166b2acdb1901d06ae1ab
Bobadu <kacperlesniewski165@gmail.com>
2025-03-26 17:10:45 +0100

    Ex. 10

---
+++ 10. Powtarzalne zadania z pętlami/script.js
@@ -0,0 +1,23 @@
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log('Counting completed!');
+
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+console.log('Countdown finished!');
+
+let i = 1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+console.log('Counting completed!');
+
+i = 5;
+while (i >= 1) {
+    console.log(i);
+    i = i - 1;
+}
+console.log('Countdown finished!');
