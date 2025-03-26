6ea7d033de730775ca54b023b6ca4aa70b5c5629
Bobadu <kacperlesniewski165@gmail.com>
2025-03-26 17:42:33 +0100

    Ex. 13

---
+++ 13. Tworzenie tablic i obiektów/script.js
@@ -0,0 +1,14 @@
+let clothes = [];
+
+clothes.push("T-shirt", "Jeans", "Jacket", "Sweater", "Sneakers");
+clothes.pop();
+clothes.push("Boots");
+
+console.log(clothes[2]);
+
+let favCar = {};
+
+favCar.color = "red";
+favCar.convertible = true;
+
+console.log(favCar);
